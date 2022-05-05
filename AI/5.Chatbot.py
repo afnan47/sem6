fa1aff9648683e77c67c380cbf2f25c05858313e
Afnan Attar <afnan.attar74@gmail.com>
2022-05-05 23:10:44 +0530

    Chatbot comments and enhancement

--- AI/5.Chatbot.py
+++ AI/5.Chatbot.py
@@ -1,12 +1,11 @@
-# Import "chatbot" from
-# chatterbot package.
+# Install Cython by using: pip install cython
+# Only then install chatterbot using: pip install chatterbot 
+
 from chatterbot import ChatBot
 
-# Inorder to train our bot, we have
-# to import a trainer package
+# Inorder to train our bot, we have to import a trainer package
 # "ChatterBotCorpusTrainer"
 from chatterbot.trainers import ChatterBotCorpusTrainer
-
 
 # Give a name to the chatbot “corona bot”
 # and assign a trainer component.
@@ -16,12 +15,11 @@ chatbot=ChatBot('corona bot')
 trainer = ChatterBotCorpusTrainer(chatbot)
 
 # Now let us train our bot with multiple corpus
-trainer.train("chatterbot.corpus.english.greetings",
-			"chatterbot.corpus.english.conversations" )
+trainer.train("chatterbot.corpus.english.greetings","chatterbot.corpus.english.conversations" )
 
 
-question = input("Enter your question bro:(Enter 0 to exit) ")
-while question != "0":
+question = input("Ask me anything:(Enter Endgame to exit) ")
+while question != "Endgame" or question != "endgame":
         response = chatbot.get_response(question)
         print(response)
         question = input("Enter your question:(Enter No to exit) ")
